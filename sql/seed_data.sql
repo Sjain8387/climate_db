b603a733f4f9750d06e8541686c20a208bd4b207
Sakshee Jain <159316764+Sjain8387@users.noreply.github.com>
2024-12-05 02:52:32 -0500

    Create seed_data.sql

---
+++ sql/seed_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO ClimateData (location, record_date, temperature, precipitation, humidity) VALUES
+('Location1', '2024-01-01', 22.5, 5.0, 60.0),
+('Location2', '2024-01-02', 18.3, 0.0, 55.0);
